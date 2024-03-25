85c7c986644ee205c90d2864dc19e45c8a879a81
arthurbcg <arthurbcg16@gmail.com>
2024-03-25 20:21:48 -0300

    finish-body-pc

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import * as S from "./styles"
 
+import { FaLaptop, FaWhatsapp, FaInstagram, FaLinkedinIn, FaGithub } from "react-icons/fa";
+
+import Avatar from "../src/assets/logo-principal.png"
+
 function App() {
   return (
     <>
       <S.Container>
-        <span>sim</span>
+        <S.Profile>
+          <img src={Avatar} />
+          <span>Devs Duo</span>
+        </S.Profile>
+        <S.Links>
+          <ul>
+            <li>
+                <a href="https://devsduo.netlify.app" target="_blank"><FaLaptop />Site</a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/devs.duo/" target="_blank"><FaInstagram />Instagram</a>
+            </li>
+            <li>
+                <a href="www.linkedin.com/company/devs-duo/" target="_blank"><FaLinkedinIn />Linkedin</a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/devs.duo/" target="_blank"><FaGithub />Github</a>
+            </li>
+            <li>
+                <a href="http://api.whatsapp.com/send?phone=55996879174" target="_blank"><FaWhatsapp /> WhatsApp (Arthur)</a>
+            </li>
+            <li>
+                <a href="http://api.whatsapp.com/send?phone=55996051394" target="_blank"><FaWhatsapp /> WhatsApp (Luiz Guilherme)</a>
+            </li>
+          </ul>
+        </S.Links>
+        <footer>
+          <span>Feito pela Devs Duo!</span>
+        </footer>
       </S.Container>
     </>
   )
--- src/styles.tsx
+++ src/styles.tsx
@@ -3,4 +3,77 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    width: 100%;
+    max-width: 588px;
+    margin: 0 auto 0;
+    padding: 0 24px;
+
+    > footer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+`;
+
+export const Profile = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    > img {
+        width: 112px;
+        border-radius: 50%;
+        cursor: pointer;
+        z-index: 999;
+        transition: all ease-in-out 0.5s;
+    }
+
+    > img:hover {
+        transform: scale(1.2);
+        filter: drop-shadow(0 0 50px #939);
+    }
+
+    > span {
+        font-weight: bolder;
+    }
+`;
+
+export const Links = styled.div`
+    > ul {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        list-style: none;
+        padding: 24px 0;
+
+        > li {
+            border-radius: 20px;
+
+            > a {
+                display: flex;
+                align-items: center;
+                padding: 16px 24px;
+                height: 30px;
+                text-decoration: none;
+                font-weight: 500;
+                background-color: #939;
+                color: white;
+                justify-content: center;
+                gap: 10px;
+                transition: all ease-in-out 0.2s;
+            }
+        }    
+
+        > li a:hover{
+            transform: scale(1.1);
+        }
+
+        > li:hover {
+            border-radius: 20px;
+            filter: drop-shadow(0 0 5px #939);
+        }
+    }
 `;
